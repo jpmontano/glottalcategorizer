ab9351c561cf7e0ad7cf3841973a52ca66ed8277
John Paul Montano <jpmontano@nmishiimnim.local>
2015-06-01 14:18:42 -0400

    Update RHSARRAY to include "-" as possible grapheme

--- glottalcategorizer.sh
+++ glottalcategorizer.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
+# Nishnaabe graphemes:
+# ', -, a, aa, b, ch, d, e, g, h, i, ii,
+# j, k, m, n, nd, ng, nh, nj, ns, ny, nzh,
+# o, p, s, sh, shk, t, w, y, z
+
+
 # Initialize the name of the file containing the glottal words to categorize.
 GLOTTALFILENAME="filewithsomeglottalwords.txt"
 
 # Declare an array of all the (regex) graphemes that could occur on the
 # left-hand side of a glottal stop.
-# 
+#
 # LHSARRAY assumes that no word can begin with a glottal stop. To
-# allow LHSARRAY to recognize glottals at the beginning of a line,
-# you can edit "[^c|n|s|z]h" to read "([^c|n|s|z]|^)h"
-LHSARRAY=("([^a]|^)a" "aa" "b" "ch" "([^n]|^)d" "e" "([^n]|^)g"
-          "[^c|n|s|z]h" "([^i]|^)i" "ii" "([^n]|^)j"
-          "([^s]|^)k" "m" "nd" "ng" "nh" "nj" "ns" "ny"
-          "([^o]|^)o" "oo" "p" "sh" "sk" "t" "w" "([^n]|^)y"
-          "zh" "'")
+# allow LHSARRAY to be open to the possibility of the occurrence of a
+# glottal stop at the beginning of a line, you can edit
+# "[^c|n|s|z]h" to read "([^c|n|s|z]|^)h"
+#
+# LHSARRAY assumes that a hyphen cannot occur immediately before a
+# glottal stop. To allow LHSARRAY to be open to the possibility
+# of the occurrence of a hyphen immediately before a glottal stop,
+# you can edit LHSARRAY to include "-"
+
+LHSARRAY=("'" "([^a]|^)a" "aa" "b" "ch" "([^n]|^)d" "e"
+          "([^n]|^)g" "[^c|n|s|z]h" "([^i]|^)i" "ii" "([^n]|^)j"
+          "([^s|sh]|^)k" "m" "nd" "ng" "nh" "nj" "ns" "ny" "nzh"
+          "([^o]|^)o" "oo" "p" "sh" "shk" "sk" "t" "w" "([^n]|^)y"
+          "([^n]|^)zh")
 
 # Declare an array of all the (regex) graphemes that could occur on the
 # right-hand side of a glottal stop.
-RHSARRAY=("a([^a]|$)" "aa" "b" "ch" "d" "e" "g" "h" "i([^i]|$)"
-          "ii" "j" "k" "m" "n([^d|g|h|j|s|y|zh]|$)" "nd" "ng"
-          "nh" "nj" "ns" "ny" "nzh" "o([^o]|$)" "oo" "p"
-          "s([^h|k]|$)" "sh([^k]|$)" "shk" "sk" "t" "w" "y"
-          "z([^h]|$)" "zh" "'")
+
+RHSARRAY=("'" "-" "a([^a]|$)" "aa" "b" "ch" "d" "e" "g" "h"
+          "i([^i]|$)" "ii" "j" "k" "m" "n([^d|g|h|j|s|y|zh]|$)"
+          "nd" "ng" "nh" "nj" "ns" "ny" "nzh" "o([^o]|$)" "oo"
+          "p" "s([^h|k]|$)" "sh([^k]|$)" "shk" "sk" "t" "w" "y"
+          "z([^h]|$)" "zh")
 
 # Get the number of elements in each array.
 NUMOFLHSELEMENTS=${#LHSARRAY[@]}
@@ -37,39 +51,39 @@ NUMOFCATS=$((0))
 # graphemes followed by a glottal followed by all orthographically
 # possible RHS graphemes.
 for ((i=0; i<$NUMOFLHSELEMENTS; i++)); do
-	for ((j=0; j<$NUMOFRHSELEMENTS; j++)); do
+    for ((j=0; j<$NUMOFRHSELEMENTS; j++)); do
 
-		# This is the glottal pattern category we're looking for.
-		GLOTPATCAT=${LHSARRAY[${i}]}h${RHSARRAY[${j}]}
+        # This is the glottal pattern category we're looking for.
+        GLOTPATCAT=${LHSARRAY[${i}]}h${RHSARRAY[${j}]}
 
-		# Array CURRENTOBS is egrepped all of the current
-		# iteration's observed occurrences of a particular
-		# glottal pattern category.
-		CURRENTOBS=( $(cat $GLOTTALFILENAME | egrep $GLOTPATCAT) )
+        # Array CURRENTOBS is egrepped all of the current
+        # iteration's observed occurrences of a particular
+        # glottal pattern category.
+        CURRENTOBS=( $(cat $GLOTTALFILENAME | egrep $GLOTPATCAT) )
 
-		# If array CURRENTOBS is not empty, then we have a
-		# newly observed glottal pattern category to display.
-		if [ "${CURRENTOBS[*]}" != "" ]
-		then
-			# We observed another unique glottal pattern category!
-			NUMOFCATS=$((NUMOFCATS + 1))
+        # If array CURRENTOBS is not empty, then we have a
+        # newly observed glottal pattern category to display.
+        if [ "${CURRENTOBS[*]}" != "" ]
+        then
+            # We observed another unique glottal pattern category!
+            NUMOFCATS=$((NUMOFCATS + 1))
 
-			# Add the newly-observed category to array OBSCATS.
-			OBSCATS+=($GLOTPATCAT)
+            # Add the newly-observed category to array OBSCATS.
+            OBSCATS+=($GLOTPATCAT)
 
-			# Display the name of the observed glottal pattern.
-			echo ------$GLOTPATCAT------
+            # Display the name of the observed glottal pattern.
+            echo ------$GLOTPATCAT------
 
-			# Display array CURRENTOBS, one element per line.
-			printf "%s\n" "${CURRENTOBS[@]}"
+            # Display array CURRENTOBS, one element per line.
+            printf "%s\n" "${CURRENTOBS[@]}"
 
-			# Display a blank line.
-			echo
+            # Display a blank line.
+            echo
 
-			# Delete array CURRENTOBS to prep for next iteration.
-			unset CURRENTOBS
-		fi
-	done
+            # Delete array CURRENTOBS to prep for next iteration.
+            unset CURRENTOBS
+        fi
+    done
 done
 
 # Display for the user a final summary of the computed results.
